1794070de43a7d6a84adedc76799a15cb762a61a
darlingson <dmakuwila@gmail.com>
2023-12-06 20:13:42 +0200

    implemented basic linked list and node

---
+++ code/data structures/arrays.py
@@ -0,0 +1,11 @@
+new_list = [1, 2, 3]
+result = new_list[0]
+
+if 1 in new_list:
+    print("1 is in the list")
+
+for n in new_list:
+    if n == 1:
+        print("1 is in the list")
+        break
+
