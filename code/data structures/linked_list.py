e750df85518e9a99f294b6994e4368d17c401543
darlingson <dmakuwila@gmail.com>
2023-12-06 20:52:16 +0200

    implemented delete in linked list

--- code/data structures/linked_list.py
+++ code/data structures/linked_list.py
@@ -35,15 +35,35 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
-    def remove(self, data):
+    
+    def insert(self, data, index):
+        if index == 0:
+            self.add(data)
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+            while position > 1:
+                current = current.next_node
+                position -= 1
+            prev_node = current
+            next_node = current.next_node
+            prev_node.next_node = new
+            new.next_node = next_node
+    def remove(self, key ):
         current = self.head
         previous = None
-        while current:
-            if current.data == data:
-                previous.next_node = current.next_node
-                current.next_node = None
-            previous = current
-            current = current.next_node
+        found = False
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+            else:
+                previous = current
+                current = current.next_node
+        return current
     def search(self, key):
         '''
         Search for the first node containing data that matches the key
@@ -73,7 +93,7 @@ class LinkedList:
         return '-> '.join(nodes)
     
 
-# insert Test
+# aa Test
 l = LinkedList()
 l.add(1)
 l.add(2)
